3c38b553f8091b42951d12aecc30a09010217a96
Angad Anil Gosain <angadgosain@gmail.com>
2024-08-22 12:51:28 -0700

    Initialised Job Application Portal project

---
+++ src/main/java/com/angadgosain/firstjobapplication/FirstjobapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.angadgosain.firstjobapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjobapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjobapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angadgosain/firstjobapplication/FirstjobapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angadgosain.firstjobapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjobapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
