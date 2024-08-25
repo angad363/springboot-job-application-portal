5dced2b363f5e2cb772ec1d747de2bbf556103ce
Angad Anil Gosain <angadgosain@gmail.com>
2024-08-25 10:55:16 -0700

    Added RequestMapping to keep base URL to "/jobs"

--- src/main/java/com/angadgosain/firstjobapplication/job/JobController.java
+++ src/main/java/com/angadgosain/firstjobapplication/job/JobController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
 
     private JobService jobService;
@@ -17,18 +18,18 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping("/jobs")
+    @GetMapping
     public ResponseEntity<List<Job>> findAll(){
         return ResponseEntity.ok(jobService.findAll());
     }
 
-    @PostMapping("/jobs")
+    @PostMapping
     public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
         return new ResponseEntity<>("Job created successfully", HttpStatus.OK);
     }
 
-    @GetMapping("/jobs/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Job> getJobById(@PathVariable Long id){
 
         Job job = jobService.getJobById(id);
@@ -38,7 +39,7 @@ public class JobController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/jobs/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<String> deleteJobById(@PathVariable Long id){
        boolean deleted = jobService.deleteJobById(id);
        if(deleted){
@@ -47,7 +48,7 @@ public class JobController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/jobs/{id}")
+    @PutMapping("{id}")
     public ResponseEntity<String> updateJobByID(@PathVariable Long id, @RequestBody Job updatedJob){
         boolean updated = jobService.updateJobById(id, updatedJob);
         if(updated){
